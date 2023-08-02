b4d007959a414faf0fc5331b7957c57b72394f79
iamsid47 <siddheshkulthe43@gmail.com>
2023-08-02 18:48:58 +0530

    defined resume data

--- main.py
+++ main.py
@@ -24,3 +24,39 @@ names = [
     "Noah Alvarez", "Olivia Garcia", "Owen Hernandez", "Peyton Lopez", "Rebecca Martinez",
     "Ryan Sanchez"
 ]
+
+def generate_resume():
+    for name in names[:2]:
+        rand_phone = openai.Completion.create(model = "text-davinci-003", prompt="Generate a random phone number of any country:", max_tokens=15)
+        rand_email = openai.Completion.create(model = "text-davinci-003", prompt=f"Generate a random email address with the name {name} :", max_tokens=15)
+        rand_loc = openai.Completion.create(model = "text-davinci-003", prompt="Generate a random address of any country:", max_tokens=15)
+        rand_sum =openai.Completion.create(model = "text-davinci-003", prompt="Generate a work summary for the job post of an Electrical Engineer", max_tokens=256)
+        rand_education =openai.Completion.create(model = "text-davinci-003", prompt="Generate an education background for the job post of an Electrical Engineer", max_tokens=256)
+        rand_skills = openai.Completion.create(model = "text-davinci-003", prompt="Generate a random skill set for the job post of an electrical engineer", max_tokens=128)
+        rand_achieve = openai.Completion.create(model = "text-davinci-003", prompt="Generate random achievements of competition or events", max_tokens=256)
+        rand_work_exp = openai.Completion.create(model = "text-davinci-003", prompt="Generate a random work experience related to electrical engineering", max_tokens=256)
+        rand_proj =openai.Completion.create(model = "text-davinci-003", prompt="Generate random projects idea list with a small description of 50 words or less for electrical engineer background", max_tokens=256)
+
+        resume_content = f"""
+        {name}
+        Contact: {rand_phone} | {rand_email}
+        Location: {rand_loc}
+
+        Summary:
+        {rand_sum}
+
+        Experience: {rand_work_exp}
+
+        Education:
+        {rand_education}
+
+        Skills:
+        {rand_skills}
+
+        Achievements: {rand_achieve}
+
+        Projects: {rand_proj}
+        """
+
+        return resume_content
+    
